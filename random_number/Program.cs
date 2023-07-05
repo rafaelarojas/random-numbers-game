0cb95733e95d869d76ce9692906026ddb19b418a
rafaelarojas <rafaelacrisr@gmail.com>
2023-07-04 22:13:41 -0300

    fix: code

---
+++ random_number/Program.cs
@@ -0,0 +1,37 @@
+﻿Random random = new Random();
+int num = random.Next(1, 101);
+
+void Attempt()
+{
+    bool acertou = false;
+
+    while (!acertou)
+    {
+        Console.Write("Digite sua tentativa de número: ");
+        string attempt = Console.ReadLine();
+        int attemptNum;
+
+        if (int.TryParse(attempt, out attemptNum))
+        {
+            if (attemptNum > num)
+            {
+                Console.WriteLine(attempt + " é maior que o número aleatório.");
+            }
+            else if (attemptNum < num)
+            {
+                Console.WriteLine(attempt + " é menor que o número aleatório.");
+            }
+            else
+            {
+                Console.WriteLine("Você acertou! O número aleatório foi " + num);
+                acertou = true;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Digite um número válido.");
+        }
+    }
+}
+
+Attempt();
